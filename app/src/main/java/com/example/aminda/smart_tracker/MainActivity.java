c497073f6c8cce047f1160ea5091be47fa89ea91
aminda007 <aminda.14@cse.mrt.ac.lk>
2018-05-21 00:32:37 +0530

    add current location to default fragment

--- app/src/main/java/com/example/aminda/smart_tracker/MainActivity.java
+++ app/src/main/java/com/example/aminda/smart_tracker/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.aminda.smart_tracker;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -16,14 +27,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.aminda.smart_tracker.Fragments.BusFragment;
+import com.example.aminda.smart_tracker.Fragments.GMapFragment;
 import com.example.aminda.smart_tracker.Fragments.TrainFragment;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, com.google.android.gms.location.LocationListener{
 
     private FragmentManager fm;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mLocation;
+    private LocationRequest mLocationRequest;
+    private com.google.android.gms.location.LocationListener listener;
+    private long UPDATE_INTERVAL = 2 * 1000;  /* 10 secs */
+    private long FASTEST_INTERVAL = 2000; /* 2 sec */
+    private LocationManager locationManager;
+    private LocationManager mLocationManager;
+    private MapFragment mapFragment;
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,6 +80,18 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
         fm = getSupportFragmentManager();
+        showMapFragment();
+
+        GMapFragment.setActivity(this);
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mLocationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        checkLocation();
     }
 
     @Override
@@ -88,42 +130,187 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
+        hideMapFragment();
+
         int id = item.getItemId();
-        String id_str = Integer.toString(id);
-        Log.d("dccxsxs","dassssss " + id);
-        FragmentTransaction ft = fm.beginTransaction();
+        Fragment fragment = null;
+
         if (id == R.id.nav_bus) {
-            if(fm.findFragmentByTag(id_str) == null){
-                ft.add(R.id.screen_area, new BusFragment(), id_str);
-                Log.d("rht","aaaaaaaaaaaaaaaaaaaa.....create bus ....aaaaaaaaaaaaaaaaaaaaaa***");
-            }
-            else{
-                ft.replace(R.id.screen_area, fm.findFragmentByTag(id_str), id_str);
-                Log.d("rht","aaaaaaaaaaaaaaaaaaaa.....resume bus ....aaaaaaaaaaaaaaaaaaaaaa***");
-            }
+            fragment = new BusFragment();
         } else if (id == R.id.nav_train) {
-            if(fm.findFragmentByTag(id_str) == null){
-                ft.replace(R.id.screen_area, new TrainFragment(), id_str);
-                Log.d("rht","aaaaaaaaaaaaaaaaaaaa.....create train ....aaaaaaaaaaaaaaaaaaaaaa***");
-            }
-            else{
-                ft.replace(R.id.screen_area, fm.findFragmentByTag(id_str), id_str);
-                Log.d("rht","aaaaaaaaaaaaaaaaaaaa.....resume train ....aaaaaaaaaaaaaaaaaaaaaa***");
-            }
+            fragment = new TrainFragment();
+        } else if (id == R.id.nav_staff) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.nav_school) {
 
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
 
         }
-        ft.commit();
+
+        if(fragment != null){
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.screen_area, fragment);
+            ft.commit();
+        }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    public void showMapFragment(){
+        FragmentTransaction ft = fm.beginTransaction();
+        if(fm.findFragmentByTag("mapFragment") == null){
+            ft.add(R.id.screen_area, new GMapFragment(), "homeFragment");
+        }else{
+            ft.show(fm.findFragmentByTag("mapFragment"));
+            Log.d("rht","aaaaaaaaaaaaaaaaaaaa.....resume map ....aaaaaaaaaaaaaaaaaaaaaa***");
+        }
+        ft.commit();
+    }
+
+    public void hideMapFragment(){
+        FragmentTransaction ft = fm.beginTransaction();
+        if(fm.findFragmentByTag("mapFragment") == null){
+
+        }else{
+            ft.hide(fm.findFragmentByTag("mapFragment"));
+            Log.d("rht","aaaaaaaaaaaaaaaaaaaa.....hide map ....aaaaaaaaaaaaaaaaaaaaaa***");
+        }
+        ft.commit();
+    }
+
+    public MapFragment initMap() {
+        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        return mapFragment;
+    }
+
+    public void onLocationChanged(Location location) {
+//        Toast.makeText(this, "msg", Toast.LENGTH_SHORT).show();
+        GMapFragment.updateLocation(location, "user");
+    }
+
+    private boolean checkLocation() {
+        if(!isLocationEnabled())
+            showAlert();
+        return isLocationEnabled();
+    }
+
+
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location")
+                .setMessage("Please Enable Location to " +
+                        "use this app")
+                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+
+                    }
+                });
+        dialog.show();
+    }
+
+    private boolean isLocationEnabled() {
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        startLocationUpdates();
+
+        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if(mLocation == null){
+            startLocationUpdates();
+        }
+        if (mLocation != null) {
+            GMapFragment.updateLocation(mLocation, "user");
+
+        } else {
+            Toast.makeText(this, "Location not Detected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Connection Suspended");
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i(TAG, "Connection failed. Error: " + connectionResult.getErrorCode());
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
+        }
+    }
+
+    protected void startLocationUpdates() {
+        // Create the location request
+        mLocationRequest = LocationRequest.create()
+                .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+                .setInterval(UPDATE_INTERVAL)
+                .setFastestInterval(FASTEST_INTERVAL);
+        // Request location updates
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            Log.d("iemi exception", "--->>>> imei no ermission graned ------------------------");
+            return;
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,
+                mLocationRequest, this);
+        Log.d("reque", "--->>>>");
+    }
+
+
 }
